1f9a1f7d05d6bd314a15b7e8bd86ddc427a3a763
Adrian Czuczka <adrian.czuczka@deliveroo.co.uk>
2020-11-07 12:33:03 +0000

    Added react-bootstrap.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import './App.css';
 import Home from './pages/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: FC = () => {
   return <Home />;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,28 @@
 import React, { FC } from 'react';
+import Card from 'react-bootstrap/esm/Card';
+import Accordion from 'react-bootstrap/esm/Accordion';
 
 const Home: FC = () => {
-  return <></>;
+  return (
+    <Accordion>
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey="0">
+          Click me!
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>Hello! Im the body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey="1">
+          Click me!
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="1">
+          <Card.Body>Hello! Im another body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  );
 };
 
 export default Home;
