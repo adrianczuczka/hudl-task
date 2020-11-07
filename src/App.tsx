fac080db7d253212a4cc9455412a1aac6a681f9f
Adrian Czuczka <adrian.czuczka@deliveroo.co.uk>
2020-11-07 19:20:25 +0000

    Added react-router-dom to project.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,23 @@ import React, { FC } from 'react';
 import './App.css';
 import Home from './pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App: FC = () => {
-  return <Home />;
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/:fixture_id">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
